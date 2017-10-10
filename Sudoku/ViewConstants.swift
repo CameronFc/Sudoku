82e23db74cbb5f5bedabc21f32b24339358f2b72
Cameron Francis <m35191970@gmail.com>
2017-10-09 23:42:41 -0300

    Cleaned up boardViewController

--- Sudoku/ViewConstants.swift
+++ Sudoku/ViewConstants.swift
@@ -11,9 +11,9 @@ import UIKit
 
 struct GameConstants {
     static private let borderSize = 1.0
-    static private let extraCellSize = 1.0
     static private let numberPickerBorderWidth = 1.0
     static private let numberPickerCellWidth = 50.0
+    static let extraCellSize = 1.0
     static let boardCellSize = 35.0
     static let totalBoardSize = CGFloat(boardCellSize * 9 + borderSize * 2 + 6 * extraCellSize)
     static let totalPickerWidth = CGFloat(3 * numberPickerCellWidth + 2 * numberPickerBorderWidth)
